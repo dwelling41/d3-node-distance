929b7cac20201e4917d15bf379521a47d154fd2e
David Welling <david.welling.41@gmail.com>
2016-06-09 00:08:02 -0400

    Code cleanup

--- js/audioDataScatterPlot.js
+++ js/audioDataScatterPlot.js
@@ -1,6 +1,5 @@
-
-var DEGREES_TO_RADIANS = Math.PI/180;
-var RADIANS_TO_DEGREES = 180/Math.PI;
+var DEGREES_TO_RADIANS = Math.PI / 180;
+var RADIANS_TO_DEGREES = 180 / Math.PI;
 
 // Determines if a node is being dragged
 var isNodeDragging = false;
@@ -23,7 +22,12 @@ var links = [];
 
 // Setup height and width
 var MAX_HEIGHT = 400;
-var margin = { top: 25, right: 25, bottom: 25, left: 25 };
+var margin = {
+    top: 25,
+    right: 25,
+    bottom: 25,
+    left: 25
+};
 var outerWidth = $('#scatter').width();
 var outerHeight = MAX_HEIGHT;
 var width = outerWidth - margin.left - margin.right;
@@ -37,8 +41,12 @@ var force = d3.layout.force()
     .on("tick", tick);
 
 var drag = force.drag()
-  .on('dragstart', function() { isNodeDragging = true;})
-  .on('dragend', function() { isNodeDragging = false;})
+    .on('dragstart', function() {
+        isNodeDragging = true;
+    })
+    .on('dragend', function() {
+        isNodeDragging = false;
+    })
 
 
 var svg = d3.select("#scatter").append("svg")
@@ -49,172 +57,195 @@ var svg = d3.select("#scatter").append("svg")
 var link = svg.selectAll(".link"),
     node = svg.append('svg:g').selectAll('g');
 
-  force
-      .nodes(nodes)
-      .links(links)
+force
+    .nodes(nodes)
+    .links(links)
 
 
 
 function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    link.attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
 
-  node.attr('transform', function(d) {
-    return 'translate(' + d.x + ',' + d.y + ')';
-  });
+    node.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
+    });
 }
 
 
 function drawContent() {
-  // Add/update Links
-  link = link.data(links);
-  link.enter().insert("line", ".node").attr("class", "link");
-  link.exit().remove();
+    // Add/update Links
+    link = link.data(links);
+    link.enter().insert("line", ".node").attr("class", "link");
+    link.exit().remove();
 
-  // Get node data
-  node = node.data(nodes, function(d) { return d.id; });
+    // Get node data
+    node = node.data(nodes, function(d) {
+        return d.id;
+    });
 
-  // update existing nodes 
-  node.selectAll('circle')
-    .attr('fill', function(d) { return d.nodeColor; })
-    .classed('selected', function(d) { return d === currentlySelectedNode; });
-    ;
-
-
-  node.selectAll('text.nodeWeight')
-    .attr("x", 0)
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .attr('class', 'nodeWeight')
-    .text(function(d) { return d.nodeWeight; });
-
-  node.selectAll('text.label')
-    .attr("x", 30)
-    .attr("dy", ".35em")
-    .attr('class', 'label')
-    .text(function(d) { return d.label; });
-
-  // add new nodes
-  var g = node.enter()
-    .append('svg:g')
-    .classed('nodeContainer', true)
-    .classed('draggable', true)
-    .on('contextmenu',onRightClickNode)
-    .call(drag);
-
-  g.append('svg:circle')
-    .attr('fill', function(d) { return d.nodeColor; })
-    .attr('class', 'node')
-    .attr('r', 25)
-
-  g.append('svg:text')
-    .attr("x", 0)
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .attr('class', 'nodeWeight')
-    .text(function(d) { return d.nodeWeight; });
-
-  g.append('svg:text')
-    .attr("x", 30)
-    .attr("dy", ".35em")
-    .attr('class', 'label')
-    .text(function(d) { return d.label; });
+    // update existing nodes 
+    node.selectAll('circle')
+        .attr('fill', function(d) {
+            return d.nodeColor;
+        })
+        .classed('selected', function(d) {
+            return d === currentlySelectedNode;
+        });;
 
 
-  // remove old nodes
-  node.exit().remove();
+    node.selectAll('text.nodeWeight')
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr('class', 'nodeWeight')
+        .text(function(d) {
+            return d.nodeWeight;
+        });
 
-  // Run the update
-  force.start();
+    node.selectAll('text.label')
+        .attr("x", 30)
+        .attr("dy", ".35em")
+        .attr('class', 'label')
+        .text(function(d) {
+            return d.label;
+        });
+
+    // add new nodes
+    var g = node.enter()
+        .append('svg:g')
+        .classed('nodeContainer', true)
+        .classed('draggable', true)
+        .on('contextmenu', onRightClickNode)
+        .call(drag);
+
+    g.append('svg:circle')
+        .attr('fill', function(d) {
+            return d.nodeColor;
+        })
+        .attr('class', 'node')
+        .attr('r', 25)
+
+    g.append('svg:text')
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr('class', 'nodeWeight')
+        .text(function(d) {
+            return d.nodeWeight;
+        });
+
+    g.append('svg:text')
+        .attr("x", 30)
+        .attr("dy", ".35em")
+        .attr('class', 'label')
+        .text(function(d) {
+            return d.label;
+        });
+
+
+    // remove old nodes
+    node.exit().remove();
+
+    // Run the update
+    force.start();
 }
 
 
 
 function onContentMousedown() {
-  // Make sure a node isn't being dragged
-  if(isNodeDragging) {
-    return;
-  };
+    // Make sure a node isn't being dragged
+    if (isNodeDragging) {
+        return;
+    };
 
-  // make sure we aren't in edit mode
-  if(inEditMode) {
-    return;
-  }
+    // make sure we aren't in edit mode
+    if (inEditMode) {
+        return;
+    }
 
-  // Validate inputs and exit if invalid
-  var error = validateNodeManagementControls();
-  if(error.length > 0) {
-    toastr.error(error);
-    return;
-  };
+    // Validate inputs and exit if invalid
+    var error = validateNodeManagementControls();
+    if (error.length > 0) {
+        toastr.error(error);
+        return;
+    };
 
-  // Get the nodes position
-  var point = d3.mouse(this)
+    // Get the nodes position
+    var point = d3.mouse(this)
 
-  // Get the node color
-  var nodeColor = getInitialColor(point[0], point[1]);
+    // Get the node color
+    var nodeColor = getInitialColor(point[0], point[1]);
 
-  // Create the new node
-  var node = {
-    id: ++lastNodeId,
-    x: point[0],
-    y: point[1],
-    fixed: true,
-    label: $('#labelInput').val(),
-    nodeWeight: parseInt($('#weightInput').val()),
-    nodeColor: nodeColor
-  };
+    // Create the new node
+    var node = {
+        id: ++lastNodeId,
+        x: point[0],
+        y: point[1],
+        fixed: true,
+        label: $('#labelInput').val(),
+        nodeWeight: parseInt($('#weightInput').val()),
+        nodeColor: nodeColor
+    };
 
-  // Add the node to the tracked list
-  nodes.push(node);
+    // Add the node to the tracked list
+    nodes.push(node);
 
-  // Clear the input
-  resetNodeManagementControls();
+    // Clear the input
+    resetNodeManagementControls();
 
-  // Redraw
-  drawContent();
+    // Redraw
+    drawContent();
 
-  toastr.success("Node created!");
+    toastr.success("Node created!");
 }
 
 
 // [min, max] => [floor, ceil]
 // ((ceil - floor) * (x - minimum))/(maximum - minimum) + floor
-function normalize(value, min, max, floor, ceiling ){
-  return ((ceiling - floor) * (value - min))/(max - min) + floor;
+function normalize(value, min, max, floor, ceiling) {
+    return ((ceiling - floor) * (value - min)) / (max - min) + floor;
 }
 
 function getInitialColor(x, y) {
-  // Map our coordinates of [0, Width] and [0, Height] to [-W/2, W/2] and [-H/2, H/2] to give a full range of HSL angles
-  var coord_x = normalize(x, 0, width, -width/2, width/2);
-  var coord_y = normalize(y, 0, height, height/2, -height/2);
+    // Map our coordinates of [0, Width] and [0, Height] to [-W/2, W/2] and [-H/2, H/2] to give a full range of HSL angles
+    var coord_x = normalize(x, 0, width, -width / 2, width / 2);
+    var coord_y = normalize(y, 0, height, height / 2, -height / 2);
 
-  // Get polar coordinates
-  var radius = Math.sqrt(Math.pow(coord_x, 2) + Math.pow(coord_y, 2));
-  var max_radius = Math.sqrt(Math.pow(width/2, 2) + Math.pow(height/2, 2));
-  var radius_normalized = normalize(radius, 0, max_radius, 0, 100);
+    // Get polar coordinates
+    var radius = Math.sqrt(Math.pow(coord_x, 2) + Math.pow(coord_y, 2));
+    var max_radius = Math.sqrt(Math.pow(width / 2, 2) + Math.pow(height / 2, 2));
+    var radius_normalized = normalize(radius, 0, max_radius, 0, 100);
 
-  var theta =  Math.atan2(coord_y, coord_x);
-  var theta_360 = (theta > 0 ? theta : (2*Math.PI + theta)) * 360 / (2*Math.PI)
+    var theta = Math.atan2(coord_y, coord_x);
+    var theta_360 = (theta > 0 ? theta : (2 * Math.PI + theta)) * 360 / (2 * Math.PI)
 
-  // Return the HSL as hex
-  var hslString = "hsl(" + theta_360 + ", " + radius_normalized + "%," + "50%)"; 
-  var color = "#" + tinycolor(hslString).toHex();
+    // Return the HSL as hex
+    var hslString = "hsl(" + theta_360 + ", " + radius_normalized + "%," + "50%)";
+    var color = "#" + tinycolor(hslString).toHex();
 
-  console.log(color);
-  return color;
+    console.log(color);
+    return color;
 }
 
 
 function onRightClickNode(clickedNode) {
-  // Stop normal events
-  d3.event.preventDefault();
+    // Stop normal events
+    d3.event.preventDefault();
 
     // Exit if dragging
-    if(isNodeDragging) {
-      return;
+    if (isNodeDragging) {
+        return;
     };
 
 
@@ -238,123 +269,123 @@ function onRightClickNode(clickedNode) {
 }
 
 function initialize() {
-  // Setup color picker
-  $('#colorInput').colorpicker();
+    // Setup color picker
+    $('#colorInput').colorpicker();
 
-  // Set default values
-  resetNodeManagementControls();
+    // Set default values
+    resetNodeManagementControls();
 
-  // Setup event handlers
-  $('#saveEditBtn').on('click', onSaveEdit);
- $('#cancelEditBtn').on('click', onCancelEdit);
- $('#deleteEditBtn').on('click', onDeleteEdit);
+    // Setup event handlers
+    $('#saveEditBtn').on('click', onSaveEdit);
+    $('#cancelEditBtn').on('click', onCancelEdit);
+    $('#deleteEditBtn').on('click', onDeleteEdit);
 
-drawContent();
-   
+    drawContent();
+
 }
 
 function onSaveEdit() {
-  // Validate inputs and exit if invalid
-  var error = validateNodeManagementControls();
-  if(error.length > 0) {
-    toastr.error(error);
-    return;
-  };
+    // Validate inputs and exit if invalid
+    var error = validateNodeManagementControls();
+    if (error.length > 0) {
+        toastr.error(error);
+        return;
+    };
 
-  // Save off values
-  currentlySelectedNode.label = $('#labelInput').val();
-  currentlySelectedNode.nodeWeight = parseInt($('#weightInput').val());
-  currentlySelectedNode.nodeColor = $('#colorInput').colorpicker('getValue');
+    // Save off values
+    currentlySelectedNode.label = $('#labelInput').val();
+    currentlySelectedNode.nodeWeight = parseInt($('#weightInput').val());
+    currentlySelectedNode.nodeColor = $('#colorInput').colorpicker('getValue');
 
-  // Clear the input
-  resetNodeManagementControls();
+    // Clear the input
+    resetNodeManagementControls();
 
-  currentlySelectedNode = null;
+    currentlySelectedNode = null;
 
-  // Redraw
-  drawContent();
+    // Redraw
+    drawContent();
 
-  toastr.success("Save successful!");
+    toastr.success("Save successful!");
 
 }
 
 function onCancelEdit() {
-  // Clear currently selected
-  currentlySelectedNode = null;
+    // Clear currently selected
+    currentlySelectedNode = null;
 
-  // Clear the input
-  resetNodeManagementControls();
+    // Clear the input
+    resetNodeManagementControls();
 
-  // Redraw
-  drawContent();
+    // Redraw
+    drawContent();
 }
 
 function onDeleteEdit() {
-  // Confirm
-  if(!confirm("Are you sure you want to delete this node?")) {
-    return;
-  }
+    // Confirm
+    if (!confirm("Are you sure you want to delete this node?")) {
+        return;
+    }
 
-  // Delete the node
-  var nodeIndex = _.findIndex(nodes, currentlySelectedNode);
-  if(nodeIndex >= 0) {
-    nodes.splice(nodeIndex, 1);
-  };
+    // Delete the node
+    var nodeIndex = _.findIndex(nodes, currentlySelectedNode);
+    if (nodeIndex >= 0) {
+        nodes.splice(nodeIndex, 1);
+    };
 
-  // Delete all associated links
-  links = _.filter(links, function(curLink) {
-    return curLink.source.id != currentlySelectedNode.id && curLink.target.id != currentlySelectedNode.id;
-  });
+    // Delete all associated links
+    links = _.filter(links, function(curLink) {
+        return curLink.source.id != currentlySelectedNode.id && curLink.target.id != currentlySelectedNode.id;
+    });
 
-  // Clear the input
-  resetNodeManagementControls();
+    // Clear the input
+    resetNodeManagementControls();
 
-  // Redraw
-  drawContent();
+    // Redraw
+    drawContent();
 
 }
 
 
 function resetNodeManagementControls() {
-  // Setup initial control values
-  $('#weightInput').val('100');
-  $('#labelInput').val('tag_' + (lastNodeId + 1));
-  $('#saveEditBtn').hide();
-  $('#cancelEditBtn').hide();
-  $('#deleteEditBtn').hide();
-  $('#colorInputGroup').hide();
-  $('#colorInput').colorpicker('setValue', '#00aabb');
-  inEditMode = false;
+    // Setup initial control values
+    $('#weightInput').val('100');
+    $('#labelInput').val('tag_' + (lastNodeId + 1));
+    $('#saveEditBtn').hide();
+    $('#cancelEditBtn').hide();
+    $('#deleteEditBtn').hide();
+    $('#colorInputGroup').hide();
+    $('#colorInput').colorpicker('setValue', '#00aabb');
+    inEditMode = false;
 }
 
 
 function validateNodeManagementControls() {
-  // Verify weight input set between 0 and 100
-  var weightInput = parseInt($('#weightInput').val());
-  if(!weightInput || weightInput < 0 || weightInput > 100) {
-    return 'Please provide a valid weight input between 0 and 100';
-  };
+    // Verify weight input set between 0 and 100
+    var weightInput = parseInt($('#weightInput').val());
+    if (!weightInput || weightInput < 0 || weightInput > 100) {
+        return 'Please provide a valid weight input between 0 and 100';
+    };
 
-  // Verify label is set
-  var labelInput = $('#labelInput').val();
-  if(!labelInput || labelInput.length <= 0) {
-    return 'Please provide a label';
-  };
+    // Verify label is set
+    var labelInput = $('#labelInput').val();
+    if (!labelInput || labelInput.length <= 0) {
+        return 'Please provide a label';
+    };
 
-  // Verify label doesn't exist
-  var existingLabelIndex = _.findIndex(nodes, function(curNode) {
-    // Ignore current node if one is selected
-    return currentlySelectedNode
-      ? curNode.label === labelInput && curNode != currentlySelectedNode
-      : curNode.label === labelInput;
-  });
+    // Verify label doesn't exist
+    var existingLabelIndex = _.findIndex(nodes, function(curNode) {
+        // Ignore current node if one is selected
+        return currentlySelectedNode ?
+            curNode.label === labelInput && curNode != currentlySelectedNode :
+            curNode.label === labelInput;
+    });
 
-  if(existingLabelIndex >= 0) {
-    return 'Please provide a unique label';
-  };
+    if (existingLabelIndex >= 0) {
+        return 'Please provide a unique label';
+    };
 
-  // Valid otherwise
-  return '';
+    // Valid otherwise
+    return '';
 }
 
 
